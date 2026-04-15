fba68d8860f4fbb32c84268a101182f807e0cfa7
jer3myvv2 <jeremyleanan209@gmail.com>
2026-04-15 14:51:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -43,5 +43,41 @@ const bookStore = {
     ]
 }
 
+const bookStoreTitle = document.getElementById("header");
+bookStoreTitle.textContent = bookStore.name;
+
+
+const bookList = document.getElementById("book-list");
+
+
+const deleteThis = document.getElementById("delete-this");
+if (deleteThis) {
+    deleteThis.remove();
+}
+
+
+bookStore.books.forEach(book => {
+
+    
+    const bookContainer = document.createElement("li");
+    const bookTitle = document.createElement("h3");
+    const bookAuthor = document.createElement("p");
+    const bookImage = document.createElement("img");
+
+    
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookImage.src = book.imageUrl;
+    bookImage.alt = book.title;
+
+    
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookImage);
+
+    
+    bookList.appendChild(bookContainer);
+});
+
 // Write your code here!
 
